ea91e679502a2e9a0e6a4f740a48635a035c4489
Andy Taylor <krumphau@gmail.com>
2019-01-15 13:02:51 +0000

    Updated floor classifier
    Greater accuracy it now uses:
     loss='categorical_crossentropy', optimizer='nadam', metrics=['categorical_accuracy']
    
    Which appear to be the best combination, yielding 99.24% accuracy

--- classifier/floor_classifier.py
+++ classifier/floor_classifier.py
@@ -12,23 +12,34 @@ from keras.preprocessing import sequence
 from keras.utils import to_categorical
 import tensorflowjs as tfjs
 
-#class 0 is dewey shelfmarks
-#class 1 is LOC shelfmarks
-
-
 #DB query to get dataset and establish classes
 
 conn = sqlite3.connect('libraryMapsFilter.db')
 cursor = conn.cursor()
 cursor2 = conn.cursor()
-query = "SELECT DISTINCT(floor_num_catalogue) FROM floor_numbers WHERE floor_num_catalogue LIKE '%STANDARD LOAN%' AND floor_num_catalogue NOT LIKE '%EAS%'  OR floor_num_catalogue LIKE '%REFERENCE%'  AND floor_num_catalogue NOT LIKE '%EAS%';"
+query = """SELECT DISTINCT '1', (floor_num_catalogue)
+                      FROM floor_numbers
+                      WHERE floor_num_catalogue LIKE '%1%'
+           UNION
+           SELECT DISTINCT '2', (floor_num_catalogue)
+                      FROM floor_numbers
+                      WHERE floor_num_catalogue LIKE '%2%'
+           UNION
+           SELECT DISTINCT '3', (floor_num_catalogue)
+                      FROM floor_numbers
+                      WHERE floor_num_catalogue LIKE '%3%' AND floor_num_catalogue NOT LIKE 'EAS%'
+           UNION
+           SELECT DISTINCT '4', (floor_num_catalogue)
+                      FROM floor_numbers
+                      WHERE floor_num_catalogue LIKE '%4%'
+           ;"""
 num_classes = []
 result_set = []
 for result in cursor.execute(query):
     print (result)
     num_classes.append(result)
     this_class = num_classes.index(result)
-    query2 = "SELECT identifier FROM floor_numbers WHERE floor_num_catalogue = '"+result[0]+"';"
+    query2 = "SELECT identifier FROM floor_numbers WHERE floor_num_catalogue = '"+result[1]+"';"
     results = cursor2.execute(query2)
     for r in results:
         result_set.append([r[0], this_class])
@@ -46,7 +57,7 @@ for r in result_set:
 # fix random seed for reproducibility
 numpy.random.seed(7)
 y = to_categorical(y)
-length_of_slice = int(len(X) * 0.9)
+length_of_slice = int(len(X) * 0.999)
 slice_object = slice(length_of_slice)
 X_train = X[slice_object]
 y_train = y[slice_object]
@@ -54,10 +65,9 @@ slice_object = slice(length_of_slice, int(len(X)))
 X_test = X[slice_object]
 y_test = y[slice_object]
 
-
 # truncate and pad input sequences
 max_review_length = 25
-top_words = 5000
+top_words = 50000
 X_train = sequence.pad_sequences(X_train, maxlen=max_review_length)
 X_test = sequence.pad_sequences(X_test, maxlen=max_review_length)
 # create the model
@@ -68,7 +78,7 @@ model.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
 model.add(MaxPooling1D(pool_size=2))
 model.add(LSTM(200))
 model.add(Dense(num_classes, activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='nadam', metrics=['categorical_accuracy'])
 print(model.summary())
 model.fit(X_train, y_train, epochs=3, batch_size=256)
 # Final evaluation of the model
